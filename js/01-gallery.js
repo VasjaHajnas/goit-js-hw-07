d088a3ed3ca98d065c5b2b963422ab685ede4765
Vasja Hajnas <129309015+VasjaHajnas@users.noreply.github.com>
2023-08-04 09:57:27 +0300

    Homework-JS7-#1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,57 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+
+function renderGalleryItem(item) {
+  return `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${item.original}">
+        <img
+          class="gallery__image"
+          src="${item.preview}"
+          data-source="${item.original}"
+          alt="${item.description}"
+        />
+      </a>
+    </li>
+  `;
+}
+
+galleryItems.forEach((item) => {
+  const galleryItemHtml = renderGalleryItem(item);
+  galleryContainer.insertAdjacentHTML("beforeend", galleryItemHtml);
+});
+
+galleryContainer.addEventListener("click", (event) => {
+  event.preventDefault();
+  const clickedElement = event.target;
+  if (clickedElement.classList.contains("gallery__image")) {
+    const largeImageUrl = clickedElement.dataset.source;
+    openModal(largeImageUrl);
+  }
+});
+
+function openModal(largeImageUrl) {
+  const instance = basicLightbox.create(`
+    <div>
+      <img src="${largeImageUrl}" alt="">
+    </div>
+  `);
+
+  instance.show();
+
+  const closeModalOnEsc = (event) => {
+    if (event.key === "Escape") {
+      instance.close();
+    }
+  };
+
+  const modalElement = instance.element();
+  modalElement.addEventListener("click", () => {
+    instance.close();
+  });
+
+  document.addEventListener("keydown", closeModalOnEsc, { once: true });
+}
+console.log(basicLightbox);
