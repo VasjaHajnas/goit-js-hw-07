5676e1c2f3f410fb4b2faf1377274139d42251a6
Vasja Hajnas <129309015+VasjaHajnas@users.noreply.github.com>
2023-08-04 12:13:32 +0300

    Homework-JS7-final

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,22 +1,19 @@
 import { galleryItems } from "./gallery-items.js";
-// Change code below this line
 
 const galleryContainer = document.querySelector(".gallery");
 
-function renderGalleryItem(item) {
-  return `
-    <li class="gallery__item">
-      <a class="gallery__link" href="${item.original}">
-        <img
-          class="gallery__image"
-          src="${item.preview}"
-          data-source="${item.original}"
-          alt="${item.description}"
-        />
-      </a>
-    </li>
-  `;
-}
+const renderGalleryItem = (item) => `
+  <li class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+      <img
+        class="gallery__image"
+        src="${item.preview}"
+        data-source="${item.original}"
+        alt="${item.description}"
+      />
+    </a>
+  </li>
+`;
 
 galleryItems.forEach((item) => {
   const galleryItemHtml = renderGalleryItem(item);
@@ -32,7 +29,7 @@ galleryContainer.addEventListener("click", (event) => {
   }
 });
 
-function openModal(largeImageUrl) {
+const openModal = (largeImageUrl) => {
   const instance = basicLightbox.create(`
     <div>
       <img src="${largeImageUrl}" alt="">
@@ -53,5 +50,6 @@ function openModal(largeImageUrl) {
   });
 
   document.addEventListener("keydown", closeModalOnEsc, { once: true });
-}
+};
+
 console.log(basicLightbox);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,10 +1,9 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
 const galleryContainer = document.querySelector(".gallery");
-
-function renderGalleryItem(item) {
-  return `
+const markup = galleryItems
+  .map((item) => {
+    return `
     <li class="gallery__item">
   <a class="gallery__link" href="${item.original}">
     <img
@@ -15,46 +14,7 @@ function renderGalleryItem(item) {
   </a>
 </li>
   `;
-}
-
-galleryItems.forEach((item) => {
-  const galleryItemHtml = renderGalleryItem(item);
-  galleryContainer.insertAdjacentHTML("beforeend", galleryItemHtml);
-});
-
-galleryContainer.addEventListener("click", (event) => {
-  event.preventDefault();
-  const clickedElement = event.target;
-  if (clickedElement.classList.contains("gallery__image")) {
-    const largeImageUrl = clickedElement.dataset.source;
-    openModal(largeImageUrl);
-  }
-});
-
-function openModal(largeImageUrl) {
-  var lightbox = new SimpleLightbox(".gallery a", {
-    captionsData: "alt",
-    captionDelay: 250,
-  });
-  `
-    <div>
-      <img src="${largeImageUrl}" alt="">
-    </div>
-  `;
-
-  instance.captions();
-
-  const closeModalOnEsc = (event) => {
-    if (event.key === "Escape") {
-      instance.close();
-    }
-  };
-
-  const modalElement = instance.element();
-  modalElement.addEventListener("click", () => {
-    instance.close();
-  });
-
-  document.addEventListener("keydown", closeModalOnEsc, { once: true });
-}
-console.log(SimpleLightbox);
+  })
+  .join("");
+galleryContainer.insertAdjacentHTML("beforeend", markup);
+new SimpleLightbox(".gallery a", { captionsData: "alt", captionDelay: 250 });
